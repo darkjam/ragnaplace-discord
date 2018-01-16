b4225f910d4fa114571d3481fc6798bb10ff0c4c
Sergio Vilar <sergiovilar.r@gmail.com>
2018-01-15 22:12:50 -0300

    Adiciona busca de skill

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const commands = new Commander(client)
 const Item = require('./item.js')
 const Mob = require('./mob.js')
 const Map = require('./map.js')
+const Skill = require('./skill.js')
 
 client.on('ready', () => {
   console.log('I am ready!');
@@ -17,16 +18,38 @@ client.on('ready', () => {
 commands.register('item', (input, message) => new Item(input, message).answer())
 commands.register('mob', (input, message) => new Mob(input, message).answer())
 commands.register('map', (input, message) => new Map(input, message).answer())
+commands.register('skill', (input, message) => new Skill(input, message).answer())
 
 // Help
 const help = (input, message) => {
-  let response = "os comandos disponíveis para busca no RagnaPlace são:\n"
-  response += "!item <nome_do_item> \n"
-  response += "!mob <nome_do_monstro> \n"
-  response += "!map <nome_do_mapa> \n"
-
-  message.reply(response)
+  message.channel.send("", {
+    "embed": {
+        color: 0x2e4089,
+        title: "Ajuda do RagnaPlace",
+        url: "https://ragnaplace.com",
+        description: "Estes são os comandos disponíveis para busca no RagnaPlace.",
+        fields: [
+          {
+              name: "Pesquisa por Monstros",
+              value: "!mob <nome ou ID do monstro>"
+          },
+          {
+              name: "Pesquisa por Itens",
+              value: "!item <nome ou ID do item>"
+          },
+          {
+              name: "Pesquisa por Mapas",
+              value: "!map <nome ou warp do mapa>"
+          },
+          {
+              name: "Pesquisa por Habilidades",
+              value: "!skill <nome da habilidade>"
+          },
+        ]
+    }
+  });
 }
+
 commands.register('ragnaplace', help)
 commands.register('database', help)
 
---
+++ skill.js
@@ -0,0 +1,11 @@
+const Ragnaplace = require('./ragnaplace.js')
+
+class Skill extends Ragnaplace {
+
+  getNamespace() {
+    return 'skill'
+  }
+
+}
+
+module.exports = Skill
